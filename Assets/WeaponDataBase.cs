1096bcd5e985f7a481dbdff8d80c84247f60e520
Tcartegnie <Sorakssis@gmail.com>
2020-02-25 15:54:08 +0100

    Add a weapon database script

---
+++ Assets/WeaponDataBase.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponDataBase : MonoBehaviour
+{
+	public WeaponDataList weaponDataList;
+	
+
+
+	public List<WeaponData> GetWeaponsList()
+	{
+		return weaponDataList.GetAllfWeapon();
+	}
+
+	public WeaponData GetWeaponByName(string name)
+	{
+		return weaponDataList.GetWeaponByName(name);
+	}
+}
