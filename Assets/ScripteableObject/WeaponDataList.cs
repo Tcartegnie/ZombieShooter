63789d0428e8dc7c9f9410911b22adccc2444fa8
Tcartegnie <Sorakssis@gmail.com>
2020-02-25 15:48:28 +0100

    Add scripteable object for manage weapons

---
+++ Assets/ScripteableObject/WeaponDataList.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Weapon", menuName = "ScriptableObjects/WeaponList", order = 1)]
+public class WeaponDataList : ScriptableObject
+{
+	[SerializeField]
+	List<WeaponData> WeaponsData;
+
+	public List<WeaponData> GetAllWeaponsOfType (WeaponType weapontype)
+	{
+		return WeaponsData.FindAll(weapons => weapontype ==  weapons.WeaponType );
+	}
+
+	public List<WeaponData>GetAllfWeapon()
+	{
+		return WeaponsData;
+	}
+
+	public WeaponData GetWeaponByName(string name)
+	{
+		return WeaponsData.Find(weapons => name == weapons.WeaponName);
+	}
+}
