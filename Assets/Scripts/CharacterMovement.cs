d5d6a10376ca7fa550db3de2664e409eb46ccecd
Tcartegnie <Sorakssis@gmail.com>
2020-02-28 17:38:17 +0100

    add a new dash systéme

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -11,11 +11,13 @@ public class CharacterMovement : MonoBehaviour
 	public bool InMove;
 	[SerializeField]
 	Animator animator;
+	public float DashDuration;
+	public float Dashdistance;
 	public float DashPower;
 	[SerializeField]
 	CharacterShoot charactershoot;
 	public int PV;
-	public float DirectionToDash;
+	public float RotationSpeed;
 	
 
 	public void StopMovement()
@@ -26,12 +28,12 @@ public class CharacterMovement : MonoBehaviour
 	public void Walk(float ForwardInput, float LateralInput)
 	{
 		Vector3 Direction = MoveForward(ForwardInput) + LateralMove(LateralInput);
+
 		Direction.Normalize();
-		transform.position += Direction * (Time.deltaTime * Speed);
 
 
 
-
+		GoToDirection(Direction);
 
 
 		InMove = true;
@@ -41,6 +43,11 @@ public class CharacterMovement : MonoBehaviour
 		animator.SetBool("InMove", InMove);
 		animator.SetFloat("Speed", Inputs.magnitude);
 
+	}
+
+	public void GoToDirection(Vector3 direction)
+	{
+		transform.position += direction * (Time.deltaTime * Speed);
 	}
 
 	public Vector3 MoveForward(float InputValue)
@@ -75,12 +82,15 @@ public class CharacterMovement : MonoBehaviour
 
 	public void LookInDirection(Vector3 direction)
 	{
-		transform.rotation = Quaternion.LookRotation(direction);
+		Quaternion targetRotation = Quaternion.LookRotation(direction);
+		transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, RotationSpeed * Time.deltaTime);
 	}
 
 	public void Dash()
 	{
-		Vector3 PointToDash = GetWalkingDirection() * DirectionToDash;
+		Vector3 PointToDash = transform.forward * Dashdistance;
+
+		StartCoroutine(Dash(transform.position + PointToDash));
 	}
 
 	public void Hit(int damage)
@@ -94,6 +104,16 @@ public class CharacterMovement : MonoBehaviour
 		if(PV <= 0)
 		{
 			Destroy(gameObject);
+		}
+	}
+
+	public IEnumerator Dash(Vector3 PointToDash)
+	{
+		Vector3 StartPosition = transform.position;
+		for(float i = 0; i < 1; i+= (Time.deltaTime / DashDuration))
+		{
+			transform.position = (Vector3.Lerp(StartPosition, PointToDash,i));
+			yield return null;
 		}
 	}
 
