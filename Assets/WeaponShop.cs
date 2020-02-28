e247bca7c00d46fe1740a299a84b3c193d85a551
Tcartegnie <Sorakssis@gmail.com>
2020-02-28 13:15:13 +0100

    Implémante the weampon shop

---
+++ Assets/WeaponShop.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponShop : MonoBehaviour
+{
+	public UIInventorySlots inventorySlots;
+	public DisplayMenuItem menuItem;
+	public WeaponDataBase weaponDataBase;
+	
+	public void TurnOnSHop()
+	{
+		List<WeaponData> weapons = weaponDataBase.GetWeaponsList();
+		menuItem.TurnOnInventory();
+		menuItem.SetItemList(weapons);
+		menuItem.DisplayItemList();
+	}
+
+	public void OnButtonSellPressed()
+	{
+		weaponDataBase.GetWeaponByName(inventorySlots.GetWeaponName(0)).SetWeaponAsBuyed();
+	}
+
+	public void OnButtonSellAnnulationPressed()
+	{
+
+	}
+
+	public void TurnOffShop()
+	{
+		menuItem.TurnOffInventory();
+	}
+
+
+}
