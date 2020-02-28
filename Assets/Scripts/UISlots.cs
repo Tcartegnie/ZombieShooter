c3d849851e889fab6ce6d9361dcdf92ea7bf68c8
Tcartegnie <Sorakssis@gmail.com>
2020-02-28 13:24:38 +0100

    Refacto the UIslots

--- Assets/Scripts/UISlots.cs
+++ Assets/Scripts/UISlots.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
-public class UISlots :ItemRect,IPointerEnterHandler
+public class UISlots :ItemRect,IPointerEnterHandler,IPointerExitHandler
 {
 	public UIInventorySlots Inventoryslots;
 	public int SlotID;
@@ -11,6 +11,11 @@ public class UISlots :ItemRect,IPointerEnterHandler
 	public void OnPointerEnter(PointerEventData eventData)
 	{
 		Inventoryslots.SelectWeaponSlot(SlotID);
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		Inventoryslots.DeselectWeaponSlot();
 	}
 
 	public void SetInventorySlotsData(WeaponData data)
