4447a5eeefc3dac674395db03a67570beda13ce2
ZephyrRaine <zephyr.raine@gmail.com>
2020-02-28 15:40:17 +0100

    colin

---
+++ Assets/Colin/BulletCollin.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCollin : MonoBehaviour
+{
+    public float speed;
+    private void Start()
+    {
+        Invoke("Destroy", 10f);
+    }
+
+    void Destroy()
+    {
+        Destroy(gameObject);
+    }
+    // Start is called before the first frame update
+    void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+}
---
+++ Assets/Colin/ColinMovement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColinMovement : MonoBehaviour
+{
+    public float speed;
+    public float dashMult;
+    public float recoilForce;
+    Rigidbody rigidbody;
+    public GameObject prefabBullet;
+    public float shootDelay;
+    float lastShoot = 0f;
+    public Transform weaponTransform;
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 dir = new Vector3();
+        dir.z = Input.GetAxis("Vertical");
+        dir.x = Input.GetAxis("Horizontal");
+        dir.Normalize();
+
+        float curSpeed = speed;
+        if(Input.GetMouseButtonDown(1))
+        {
+            curSpeed *= dashMult;
+        }
+    
+
+        dir *= curSpeed;
+        
+        if(Input.GetMouseButton(0))
+        {
+            if(TryShoot())
+            {
+                dir -= transform.forward * recoilForce;
+            }
+        }
+        
+        rigidbody.velocity = dir * Time.deltaTime;
+        
+
+        
+        if(lastShoot > 0f)
+        {
+            lastShoot -= Time.deltaTime;
+        }
+    }
+
+    bool TryShoot()
+    {
+        if(lastShoot <= 0f)
+        {
+            lastShoot = shootDelay;
+            Instantiate(prefabBullet, weaponTransform.transform.position, weaponTransform.rotation);
+            return true;
+        }
+        return false;
+
+    }
+}
