8f745ab00a0a9099f471b999c8cf6b11fc1a00d6
Tcartegnie <Sorakssis@gmail.com>
2020-03-19 22:58:55 +0100

    add the 3D model and the PNG on the weapon data

--- Assets/ScripteableObject/Weapon/WeaponData.cs
+++ Assets/ScripteableObject/Weapon/WeaponData.cs
@@ -5,21 +5,28 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Weapon", menuName = "ScriptableObjects/Weapon", order = 1)]
 public class WeaponData : ScriptableObject
 {
+	public GameObject Bullet;
+	public GameObject WeaponModel;
+	[Space]
 	public string WeaponName;
 	public WeaponType WeaponType;
 	public int Damage;
 	public float CoolDown;
 	public int LoadoutMax;
-	public float BulletOffset;
 	public float ReloadTime;
 	public int BulletPerShoot;
 	public float CoolDownBetweenBullet;
 	public int MaxAmmo;
 	public Vector2 Spread;
-	public GameObject Bullet;
 	public Sprite WeaponPicture;
+	[Space]
 	public bool Buyed;
-	public float recoil;
+	[Space]
+	public float RecoilDurantion;
+	public float RecoilDistance;
+	public AnimationCurve RecoilCurve;
+
+
 
 	public void SetWeaponAsBuyed()
 	{
