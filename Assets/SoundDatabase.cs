738278dff2296a3876c0c5d2a9cd512fcb4ab7b4
Tcartegnie <Sorakssis@gmail.com>
2020-03-24 17:35:40 +0100

    Add a new scriptable object to manage the sounds

---
+++ Assets/SoundDatabase.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundDatabase : MonoBehaviour
+{
+
+	public List<SoundsListData> soundListData;
+    // Start is called before the first frame update
+    void Start()
+    {
+		foreach(SoundsListData soundlist in soundListData)
+		{
+			soundlist.SetSoundDictionary();
+		}
+	}
+
+   public SoundsListData GetSoundListsByName (string Name)
+	{
+		return soundListData.Find(list => list.NameList == Name);
+	}
+
+	public AudioClip GetRandomSound(string SoundListName,string SoundName)
+	{
+		SoundsListData data = GetSoundListsByName(SoundListName);
+		return data.GetRandomSound(SoundName);
+	}
+}
