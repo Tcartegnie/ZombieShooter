01f2c056d8787549203b427c51f014270448d2f4
Tcartegnie <Sorakssis@gmail.com>
2020-03-24 17:03:15 +0100

    Rename the script partile player

---
+++ Assets/ParticlePlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticlePlayer : MonoBehaviour
+{
+	public ParticleSystem particle;
+
+	public void Start()
+	{
+		StartCoroutine(PlayParticle());
+	}
+
+	public IEnumerator PlayParticle()
+	{
+		particle.Play();
+		yield return new WaitForSeconds(particle.main.duration);
+		Destroy(gameObject);
+	}
+}
