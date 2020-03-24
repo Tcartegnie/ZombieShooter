581dd6231480351b16ba90c1a7a9cf6e3274b1a4
Tcartegnie <Sorakssis@gmail.com>
2020-03-24 19:20:52 +0100

    Add a recoil class

---
+++ Assets/Scripts/Recoil.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recoil : MonoBehaviour
+{
+
+	public CharacterController characterController;
+
+
+	public void PlayRecoil(WeaponData data)
+	{
+		StartCoroutine(ShootRecoil(data));
+	}
+
+	IEnumerator ShootRecoil(WeaponData data)
+	{
+		
+		float NormalizedDistanceOfRecoil = (data.RecoilDistance / data.RecoilDurantion);
+		for (float t = 0; t < data.RecoilDurantion; t += Time.deltaTime)
+		{
+			yield return null;
+			characterController.Move(-transform.forward * (NormalizedDistanceOfRecoil * Time.deltaTime) * data.RecoilCurve.Evaluate(t));
+		}
+	}
+}
