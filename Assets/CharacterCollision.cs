0b4cf0a4beeb1e2a3d73da1d8e0558700c1ca73f
Tcartegnie <Sorakssis@gmail.com>
2020-03-25 17:35:50 +0100

    Add a new class to manage the collision

---
+++ Assets/CharacterCollision.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCollision : MonoBehaviour
+{
+	public SoundPlayer Sounds;
+	public GameObject Bloodparticles;
+	public CharacterStatistique stats;
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.tag == ("Bullet"))
+		{
+			stats.Hit(collision.gameObject.GetComponent<Bullet>().GetDamage(),0);
+			Destroy(collision.gameObject);
+			BloodEmission(collision.transform);
+			Sounds.PlayRandomSound("ZombieHit");
+		}
+	}
+
+
+	public void BloodEmission(Transform ImpactPoint)
+	{
+		Instantiate(Bloodparticles, ImpactPoint.position, Quaternion.LookRotation(ImpactPoint.forward));
+	}
+}
