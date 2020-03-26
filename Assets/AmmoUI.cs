25a83b14312df3a7ca54154930ba8b9b0e53d332
Tcartegnie <Sorakssis@gmail.com>
2020-03-26 19:30:01 +0100

    Now display the amount of bullet on the screen

---
+++ Assets/AmmoUI.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AmmoUI : MonoBehaviour
+{
+	public FireWeapon CurrentWeapon;
+	public CharacterInventory characterInventory;
+	public Text AmmoQuantity;
+	public Text AmmoQuantityMax;
+	public Image WeaponPicture;
+
+	public void Update()
+	{
+		DisplayAmmo();
+		DisplayAmmoMax();
+	}
+
+	public void ChangeWeapon(FireWeapon weapon)
+	{
+		CurrentWeapon = weapon;
+		ChangeWeaponPicture(weapon.weaponData.WeaponPicture);
+	}
+
+	public void ChangeWeaponPicture(Sprite WeaponPicure)
+	{
+		WeaponPicture.sprite = WeaponPicure;
+	}
+		
+
+	public void DisplayAmmo()
+	{
+		AmmoQuantity.text = CurrentWeapon.weaponData.CurrentLoader + "/" + CurrentWeapon.weaponData.LoadoutMax;
+	}
+	
+	public void DisplayAmmoMax()
+	{
+		AmmoQuantityMax.text = characterInventory.GetCurrentAmmoQuantity() + " / " + CurrentWeapon.weaponData.MaxAmmo;
+	}
+
+}
