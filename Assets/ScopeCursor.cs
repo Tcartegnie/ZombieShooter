75534f15941b1f7a337991f1811bd0bf4e909ec1
Tcartegnie <Sorakssis@gmail.com>
2020-04-06 17:00:14 +0200

    Add a cursor in game

---
+++ Assets/ScopeCursor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ScopeCursor : MonoBehaviour
+{
+	public RectTransform CursorTransform;
+
+
+
+	private void Start()
+	{
+		Cursor.visible = false;
+	}
+	// Update is called once per frame
+	void Update()
+    {
+		CursorTransform.position = Input.mousePosition;
+	}
+
+
+
+
+
+
+
+}
