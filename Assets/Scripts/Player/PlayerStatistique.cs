95665da8c9f2c384099fd8fd261e6a3b307edd84
Tcartegnie <Sorakssis@gmail.com>
2020-04-06 16:59:21 +0200

    Rename "Character statistique" to "Player statisique"  and refactorise it

---
+++ Assets/Scripts/Player/PlayerStatistique.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStatistique : CharacterStatistique
+{
+	public LifeBar lifebar;
+
+	public delegate void OnDeath();
+
+	public OnDeath ondeath;
+
+	public override void ManageLifeBar()
+	{
+		lifebar.SetBarValue(GetComputeScore());
+	}
+
+	public override void Death()
+	{
+		ondeath();
+		gameObject.SetActive(false);
+	}
+}
