116b7651711d2eb51eb3a47a11471ef9038999df
Tcartegnie <Sorakssis@gmail.com>
2020-04-06 17:15:36 +0200

    Add file to manage the instanciations of the waves of zombie

---
+++ Assets/ZombieWaveManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieWaveManager : MonoBehaviour
+{
+	public Transform[] ZombiesSpawn;
+	public List<int> ZombieWave;
+	public PlayerStatistique playerStatistique;
+
+
+	public void Start()
+	{
+		InstanciatedZombiesWave(ZombieWave[0]);
+
+	}
+
+	public void InstanciatedZombiesWave(int ZombieNumber)
+	{
+		for(int i= 0; i < ZombieNumber;i++ )
+		{
+			InstantiateZombie();
+		}
+	}
+
+	public void InstantiateZombie()
+	{
+
+	
+	}
+}
