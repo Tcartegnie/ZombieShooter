9c85225f7ab2e513e7aaeb3b1b84f6ea1dd075de
Tcartegnie <Sorakssis@gmail.com>
2020-04-06 16:34:43 +0200

    Add new UI assets in the project

---
+++ Assets/TransitionFX.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class TransitionFX : MonoBehaviour
+{
+	public float FxDuration;
+	public abstract void PlayFX();
+	public abstract void RevertFX();
+}
