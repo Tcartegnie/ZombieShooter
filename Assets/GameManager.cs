bdebc500fca197cf3e4d8f0b7322997fb807fda9
Tcartegnie <Sorakssis@gmail.com>
2020-04-06 16:51:55 +0200

    add a game manager

---
+++ Assets/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public GameObject Player;
+	// Start is called before the first frame update
+	void Awake()
+    {
+		DontDestroyOnLoad(gameObject);
+	}
+
+	/*
+	 Charger la scéne de jeu avec le personnage du joueur deja instancié 
+	 Mettre tout les données de chargement du niveau dans un scriptable object
+
+	 */
+}
