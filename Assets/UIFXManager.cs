1976370861354648cc325183595d370c03b85434
Tcartegnie <Sorakssis@gmail.com>
2020-04-06 17:17:53 +0200

    Add a script to manage FX on the UI

---
+++ Assets/UIFXManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIFXManager : MonoBehaviour
+{
+	public List<TransitionFX> FireFx;
+
+	public void PlayFireFX()
+	{
+		foreach(TransitionFX fx in FireFx)
+		{
+			fx.PlayFX();
+		}
+	}
+
+	public void StopFireFX()
+	{
+		foreach (TransitionFX fx in FireFx)
+		{
+			fx.RevertFX();
+		}
+	}
+}
