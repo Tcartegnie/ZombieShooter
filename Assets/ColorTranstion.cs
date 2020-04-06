c0e2a1d39f0164d9575c1d12e1d173840fc2ac6c
Tcartegnie <Sorakssis@gmail.com>
2020-04-06 17:12:53 +0200

    Add some script to manage UI fx

---
+++ Assets/ColorTranstion.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorTranstion : TransitionFX
+{
+	
+
+	public Image Scope;
+
+	public Color ScopeBaseColor;
+	public Color ScopeShootColor;
+
+
+
+	public IEnumerator ColorTransition(Color ColorToFade)
+	{
+		Color color = new Color();
+		color =	Scope.color;
+		for (float i = 0; i < 1; i += Time.deltaTime / FxDuration)
+		{
+
+			Scope.color = Color.Lerp(color, ColorToFade, i);
+
+
+			yield return null;
+		}
+		Scope.color = ColorToFade;
+	}
+
+	public IEnumerator TransitToFXcolor()
+	{
+		yield return StartCoroutine(ColorTransition(ScopeShootColor));
+	}
+
+	public IEnumerator TransitToBasicColor()
+	{
+		yield return StartCoroutine(ColorTransition(ScopeBaseColor));
+
+	}
+
+	public IEnumerator ScopeShootFX()
+	{
+		yield return StartCoroutine(TransitToFXcolor());
+		yield return StartCoroutine(TransitToBasicColor());
+	}
+
+	public override void PlayFX()
+	{
+		StartCoroutine(TransitToFXcolor());
+	}
+
+	public override void RevertFX()
+	{
+		StartCoroutine(TransitToBasicColor());
+	}
+}
---
+++ Assets/ScaleTransition.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScaleTransition : TransitionFX
+{
+	public RectTransform RectToScale;
+
+	public AnimationCurve GrowingAnimationcurve;
+	public AnimationCurve FlatAnimationcurve;
+
+
+
+
+
+	public IEnumerator ScaleScope(AnimationCurve curve)
+	{
+
+		for (float i = 0; i < 1; i += Time.deltaTime / FxDuration)
+		{
+
+			float curveValue = curve.Evaluate(i);
+
+			RectToScale.localScale = new Vector3((curveValue),
+													 (curveValue),
+														0);
+
+
+			yield return null;
+		}
+		float FinalcurveValue = curve.Evaluate(1);
+
+		RectToScale.localScale = new Vector3((FinalcurveValue),
+												 (FinalcurveValue),
+													0);
+
+	}
+
+
+	public IEnumerator GrowTheScope()
+	{
+		yield return StartCoroutine(ScaleScope(GrowingAnimationcurve));
+	}
+
+	public IEnumerator FlatTheScope()
+	{
+		yield return StartCoroutine(ScaleScope(FlatAnimationcurve));
+
+	}
+
+
+	public override void PlayFX()
+	{
+
+		StartCoroutine(GrowTheScope());
+	}
+
+	public override void RevertFX()
+	{
+		StartCoroutine(FlatTheScope());
+
+	}
+
+	
+
+}
