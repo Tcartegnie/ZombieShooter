ff8b501f76b8f7381218a4cc079dca15071b2f5c
Tcartegnie <Sorakssis@gmail.com>
2020-04-06 17:14:01 +0200

    Add a zombie factory

---
+++ Assets/ZombieFactory.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ZombieFactory : MonoBehaviour
+{
+	public GameObject Zombie;
+	public SoundDatabase sounddatabase;
+
+	public void InstanciateZombie(Vector3 WorldPosition)
+	{
+		GameObject GO = Instantiate(Zombie, WorldPosition ,new Quaternion());
+		SoundPlayer sound = GO.GetComponentInChildren<SoundPlayer>();
+		sound.SoundList = sounddatabase.GetSoundListsByName(sound.ListName);
+		GO.GetComponentInChildren<Zombie>().StalkPlayer = true;
+	}
+
+}
