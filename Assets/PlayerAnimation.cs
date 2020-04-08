147bb065ff507efe51e6523b57b004618157ca00
Tcartegnie <Sorakssis@gmail.com>
2020-04-08 19:26:31 +0200

    Add new script to manage cheracter animations

---
+++ Assets/PlayerAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+public	PlayerMovement playermovement;
+public	CharacterShoot playerShoot;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+	public void GetLateralDirectionValue()
+	{
+		Vector3.Dot(playerShoot.GetShootDirection(), playermovement.GetLateralDirection(Input.GetAxis("Horizontal")));
+	}
+
+	public void GetForwardDirectionValue()
+	{
+
+	}
+}
