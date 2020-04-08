2379a0115be81d1c829bee17efb8aa9e0c2e6e6f
Tcartegnie <Sorakssis@gmail.com>
2020-04-08 19:27:12 +0200

    Refacto the Zombie Staistique script
    Remove the "Death" methode

--- Assets/Scripts/Player/ZombieStatistique.cs
+++ Assets/Scripts/Player/ZombieStatistique.cs
@@ -7,16 +7,15 @@ public class ZombieStatistique : CharacterStatistique
 {
 	public Animator ZombieClip;
 	public NavMeshAgent agent;
+	public Zombie zombie;
+	public ZombieCollision collider;
+	public Collider ColliderBullet;
 	public override void CallDeath()
 	{
 		base.CallDeath();
 		ZombieClip.enabled = false;
-	}
-
-
-	public void Death()
-	{
-		//Destroy(gameObject);
-	
+		zombie.PauseNavMeshAgent();
+		collider.Push();
+		ColliderBullet.enabled = (false);
 	}
 }
