974aa417856a6f052e9da3f21f576b393c12d45b
Tcartegnie <Sorakssis@gmail.com>
2020-04-20 19:09:21 +0200

    Refacto the debug UI

---
+++ Assets/DebugUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugUI : MonoBehaviour
+{
+	public Text zombieCount;
+	public Text zombieMultiplier;
+	
+
+	public void DisplayZombieMultiplier(float value)
+	{
+		zombieMultiplier.text = "Zombie multiplier : " + value;
+	}
+
+	public void DisplayZombieCount(float value)
+	{
+		zombieCount.text = "Zombie count : " + value;
+	}
+}
