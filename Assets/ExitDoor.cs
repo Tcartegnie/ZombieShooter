8438e6079d216914f3c5fe99175c5dcf0feadcfc
Tcartegnie <Sorakssis@gmail.com>
2020-04-21 18:38:14 +0200

    Add an trigger to exit the level

---
+++ Assets/ExitDoor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitDoor : MonoBehaviour
+{
+	public EndGameScript endGameMenu;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			endGameMenu.FadeVictory();
+		}
+	}
+}
