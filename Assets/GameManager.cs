d060b3b1f4409f3113acaf8bca67a8b4d992548f
Tcartegnie <Sorakssis@gmail.com>
2020-05-04 20:16:04 +0200

    Rework the game manager

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,12 +4,34 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+
+	public static GameManager instance;
 	public GameObject Player;
+
+
+
+
+
+	public static GameManager Instance()
+	{
+		if(instance == null)
+		{
+			instance = new GameObject("GameManager").AddComponent<GameManager>();
+			DontDestroyOnLoad(instance);
+		}
+		return instance;
+	}
+	
+
 	// Start is called before the first frame update
 	void Awake()
     {
 		DontDestroyOnLoad(gameObject);
 	}
+
+
+
 
 	/*
 	 Charger la scéne de jeu avec le personnage du joueur deja instancié 
