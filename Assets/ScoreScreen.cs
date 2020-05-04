7700d1772679581fbb21dd29779848785eea1cb3
Tcartegnie <Sorakssis@gmail.com>
2020-05-04 20:04:39 +0200

    Add button fucntion in the score scene

---
+++ Assets/ScoreScreen.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScoreScreen : MonoBehaviour
+{
+	ScoreManager SM;
+	public Text ScoreText;
+	public ScoreUI scoreUI;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		SM = ScoreManager.Instance();
+		ScoreText.text = SM.ComputeScore().ToString();
+		scoreUI.DisplayUIData();
+    }
+
+
+	public void OnMainMenuButton()
+	{
+		SceneManager.LoadScene(1);
+	}
+
+	public void OnGoToQG()
+	{
+		SceneManager.LoadScene(1);
+	}
+	
+}
